e0b48d82c8474513b86cf8d70b96b221dbe4ab6a
Tracy <tracy.tran@outlook.fr>
2020-01-22 11:48:20 +0100

    1er commit

--- lib/app.rb
+++ lib/app.rb
@@ -1,6 +1,6 @@
 require 'dotenv'# Appelle la gem Dotenv
 
-Dotenv.load('.env') # Ceci appelle le fichier .env (situé dans le même dossier que celui d'où tu exécute app.rb)
+Dotenv.load('hidden.env') # Ceci appelle le fichier .env (situé dans le même dossier que celui d'où tu exécute app.rb)
 # et grâce à la gem Dotenv, on importe toutes les données enregistrées dans un hash ENV
 
 # Il est ensuite très facile d'appeler les données du hash ENV, par exemple là je vais afficher le contenu de la clé TWITTER_API_SECRET
